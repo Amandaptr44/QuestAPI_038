a890e55df63a4238f3efe1d5902277067e86588d
github_Amandaptr44 <amanda.putri.ft22@mail.umy.ac.id>
2024-12-29 23:10:39 +0700

    menampilkan gambar atau animasi sebagai indikator error

--- app/src/main/java/com/example/pertemuan12/ui/view/mahasiswa/HomeView.kt
+++ app/src/main/java/com/example/pertemuan12/ui/view/mahasiswa/HomeView.kt
@@ -135,3 +135,21 @@ fun OnLoading(modifier: Modifier = Modifier){
     )
 }
 
+//the home screen displaying error message with re-attempt button
+@Composable
+fun OnError(retryAction: () -> Unit, modifier: Modifier = Modifier){
+    Column (
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Image(
+            painter = painterResource(id = R.drawable.error), contentDescription = ""
+        )
+        Text(text = stringResource(R.string.loading_failed), modifier = Modifier.padding(16.dp))
+        Button(onClick = retryAction) {
+            Text(stringResource(R.string.retry))
+        }
+    }
+}
+
