ea5e902d9e8cf73050033a9df3b6875bdbd82179
github_Amandaptr44 <amanda.putri.ft22@mail.umy.ac.id>
2024-12-29 23:15:30 +0700

    menyediakan factory untuk membuat instance dari ViewModel (HomeViewModel dan InsertViewModel)

---
+++ app/src/main/java/com/example/pertemuan12/ui/viewmodel/PenyediaViewModel.kt
@@ -0,0 +1,14 @@
+package com.example.pertemuan12.ui.viewmodel
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.pertemuan12.MahasiswaAplications
+
+object PenyediaViewModel{
+    val Factory = viewModelFactory {
+        initializer { HomeViewModel(aplikasiKontak().container.kontakRepository) }
+        initializer { InsertViewModel(aplikasiKontak().container.kontakRepository) }
+    }
+}
