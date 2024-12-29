d9f02c04c991d5ed850bce56354d078972e96b8d
github_Amandaptr44 <amanda.putri.ft22@mail.umy.ac.id>
2024-12-29 20:08:26 +0700

    menambahkan fun insertMhs untuk menyisipkan data mahasiswa

--- app/src/main/java/com/example/pertemuan12/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/pertemuan12/ui/viewmodel/InsertViewModel.kt
@@ -13,3 +13,18 @@ class InsertViewModel (private val mhs: MahasiswaRepository): ViewModel(){
     var uiState by mutableStateOf(InsertUiState())
         private set
 
+    fun updateInsertMhsState(insertUiEvent: InsertUiEvent){
+        uiState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
+
+    suspend fun insertMhs(){
+        viewModelScope.launch {
+            try {
+                mhs.insertMahasiswa(uiState.insertUiEvent.toMhs())
+            }catch (e:Exception){
+                e.printStackTrace()
+            }
+        }
+    }
+}
+
