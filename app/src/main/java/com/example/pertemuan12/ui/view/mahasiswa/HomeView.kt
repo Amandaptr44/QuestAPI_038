548983f44a25126c05db6b3da5edb9e80e57ecab
github_Amandaptr44 <amanda.putri.ft22@mail.umy.ac.id>
2024-12-29 23:07:25 +0700

    daftar mahasiswa yang bisa dilihat, dihapus, dan memungkinkan navigasi untuk menambah data mahasiswa baru

--- app/src/main/java/com/example/pertemuan12/ui/view/mahasiswa/HomeView.kt
+++ app/src/main/java/com/example/pertemuan12/ui/view/mahasiswa/HomeView.kt
@@ -49,3 +49,48 @@ object DestinasiHome : DestinasiNavigasi {
     override val titleRes = "Home Mhs"
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(
+    navigateToItemEntry:() -> Unit,
+    modifier: Modifier = Modifier,
+    ondetailClick: (String) -> Unit = {},
+    viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold (
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CostumeTopAppBar(
+                title = DestinasiHome.titleRes,
+                canNavigateBack = false,
+                scrollBehavior = scrollBehavior,
+                onRefresh = {
+                    viewModel.getMhs()
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = navigateToItemEntry,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(18.dp)
+            ){
+                Icon(imageVector = Icons.Default.Add, contentDescription = "Add Kontak")
+            }
+        },
+    ) { innerPadding ->
+        HomeStatus(
+            homeUiState = viewModel.mhsUIState,
+            retryAction = { viewModel.getMhs() }, modifier = Modifier.padding(innerPadding),
+            ondetailClick = ondetailClick, ondeleteClick = {
+                viewModel.deleteMhs(it.nim)
+                viewModel.getMhs()
+            }
+        )
+    }
+
+
+}
+
+
