5150f91bdb7c042f806d32d7649a3b7b3b42730a
github_Amandaptr44 <amanda.putri.ft22@mail.umy.ac.id>
2024-12-29 20:14:39 +0700

    menambahkan sealed class HomeUiState untuk mewakili berbagai kondisi di layar home

---
+++ app/src/main/java/com/example/pertemuan12/ui/viewmodel/HomeViewModel.kt
@@ -0,0 +1,19 @@
+package com.example.pertemuan12.ui.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import coil.network.HttpException
+import com.example.pertemuan12.model.Mahasiswa
+import com.example.pertemuan12.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
+import java.io.IOException
+
+sealed class HomeUiState{
+    data class Success(val mahasiswa: List<Mahasiswa>): HomeUiState()
+    object Error : HomeUiState()
+    object Loading : HomeUiState()
+}
+
