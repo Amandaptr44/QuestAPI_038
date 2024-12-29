fb703d175592aa3d514315cec93a519a2720d75e
github_Amandaptr44 <amanda.putri.ft22@mail.umy.ac.id>
2024-12-29 20:05:51 +0700

    menambahkan InsertViewModel

--- app/src/main/java/com/example/pertemuan12/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/pertemuan12/ui/viewmodel/InsertViewModel.kt
@@ -13,7 +13,3 @@ class InsertViewModel (private val mhs: MahasiswaRepository): ViewModel(){
     var uiState by mutableStateOf(InsertUiState())
         private set
 
-    fun updateInsertMhsState(insertUiEvent: InsertUiEvent){
-        uiState = InsertUiState(insertUiEvent = insertUiEvent)
-    }
-
